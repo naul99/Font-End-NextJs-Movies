024f6bfc95dcacff2fc41e57e8b5943bbb9e6f5a
naul99 <luanboss012@gmail.com>
2025-01-27 16:37:58 +0700

    build

---
+++ pages/watch/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WatchPage = () => {
+  return (
+    <div>
+      <h1>Watch Page</h1>
+      <p>This is the watch page.</p>
+    </div>
+  );
+};
+
+export default WatchPage;
